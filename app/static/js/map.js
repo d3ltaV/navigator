149ecca4a12e8091b1fb8b35d606833295ab95d1
Trashbot7274 <1w3r5yviper2077@gmail.com>
2025-11-13 18:07:26 -0500

    fixed placement of RAC pin, added blake and forest, modified csv for to group blake workjobs together

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -12,20 +12,24 @@ async function initMap() {
     };
 
     const locations = [
-        { title: "Library", position: { lat: 42.6686206, lng: -72.4834041 } },
         { title: "Gilder", position: { lat: 42.667144, lng: -72.481665 } },
         { title: "Dorms", position: { lat: 42.6676204, lng: -72.4838411 } },
         { title: "Admissions (Bolger)", position: { lat: 42.66644601940982 , lng:-72.48515196277874 } },
         { title: "Alumni Hall", position: { lat:42.66748046088102 , lng:-72.48557843400381 } },
         { title: "Schauffler Library", position: { lat: 42.66899511246868, lng:-72.48312287168545 } },
         { title: "Gym", position: { lat:42.66622766388029 , lng:-72.48156656247123 } },
-        { title: "RAC", position: { lat: 42.66894637358398, lng: -72.48241011724572 } },
+        { title: "RAC", position: { lat: 42.66798729516262, lng: -72.4816328964554 } },
         { title: "O'Connor Health Center", position: { lat: 42.66725816449024, lng: -72.4867499314032} },
         { title: "Communications office", position: { lat: 42.67012671214381, lng: -72.48195950620217} },
         { title: "Early Childhood Center", position: { lat:42.66832414764135, lng:-72.47894604438869 } },
         { title: "Farm", position: { lat:42.670343686705166, lng:-72.48129390883983 } },
         { title: "Plant Facilities", position: { lat: 42.66961202001847, lng: -72.48068772960862 } },
-        { title: "BEV", position: { lat:42.66896317632714 , lng: -72.48240568446545} }
+        { title: "BEV", position: { lat:42.66896317632714 , lng: -72.48240568446545} },
+        { title: "Forest", position: {lat: 42.67102844380024, lng: -72.48804669028948} },
+        { title: "Blake", position: {lat: 42.66851986413596, lng: -72.4847851241787} },
+
+
+
     ];
 
     await mapElement.innerMap;
